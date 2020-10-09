7974b5e7790e1dd438aeb1fc0831a60729adee72
nickrotondo <nickrotondo@gmail.com>
2020-10-09 15:12:01 -0700

    Use http-proxy-middleware to configure routes inside proxy server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public/dist')));
+
+// Nav Bar Endpoints
+app.use('/nav', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+
+// Reviews Endpoints
+app.use('/reviews', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
 
 let port = 3000;
 app.listen(port, () => console.log(`Connected and listening on port ${port}`));
