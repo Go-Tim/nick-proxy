0f0f85050412dc0aa9ef401ff9791cc51202e0ca
nickrotondo <nickrotondo@gmail.com>
2020-10-10 10:59:50 -0700

    Add url base to routes

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public/dist')));
 
 // Nav Bar Endpoints
-app.use('/nav', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/nav', createProxyMiddleware({ target: 'http://54.67.110.197:3001', changeOrigin: true }));
 
 // Reviews Endpoints
-app.use('/reviews', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: 'http://54.67.110.197:3004', changeOrigin: true }));
 
 let port = 3000;
 app.listen(port, () => console.log(`Connected and listening on port ${port}`));
